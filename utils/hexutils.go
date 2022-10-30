5f0e94b7c34bcc1313d0259dadd73ebc208eb27f
Raghavendra Balgi <rkbalgi@gmail.com>
2022-10-30 15:02:03 +0530

    chore(): Minor cleanup

--- utils/hexutils.go
+++ utils/hexutils.go
@@ -2,13 +2,13 @@ package utils
 
 import "encoding/hex"
 
-//StringToHex coverts a hex string into a []byte (binary)
+// StringToHex coverts a hex string into a []byte (binary)
 func StringToHex(hexStr string) []byte {
 	val, _ := hex.DecodeString(hexStr)
 	return val
 }
 
-//HexToString converts a binary slice into a string
+// HexToString converts a binary slice into a string
 func HexToString(hexArray []byte) string {
 	return hex.EncodeToString(hexArray)
 }
